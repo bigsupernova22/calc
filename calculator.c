fbca53136ec272aa6a2a9e1fbe90babb819d7dff
bigsupernova22@gmail.com <bigsupernova22@gmail.com>
2025-08-27 14:53:39 +0900

    modify

--- calculator.c
+++ calculator.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "arithmetic.h"
+
 
 int main(int argc, char *argv[])
 {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <num1> <num2>\n", argv[0]);
+        return 1;
+    }
+
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
 
-    printf("%d + %d = %d\n", a, b, a+b);
-    printf("%d - %d = %d\n", a, b, a-b);
-    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d + %d => %d\n", a, b, a + b);
+    printf("%d - %d => %d\n", a, b, a - b);
+    printf("%d * %d => %d\n", a, b, a * b);
+
     if (b != 0)
-        printf("%d / %d = %d\n", a, b, a/b);
+        printf("%d / %d => %d\n", a, b, a / b);
     else
         printf("Division by zero is undefined.\n");
 
